59ac274ee63da5a9d653b9137695a4df36d5f7d4
Albert <muminovali648@gmail.com>
2026-04-16 00:00:59 +0900

    feat: learn python boolean

--- primitive.py
+++ primitive.py
@@ -1,7 +1,8 @@
-print("===== number =====")
+print("===== string =====")
 # in JAVA, variable is a name storage location!
 # in Python, vRIbles is anmed reference!
 
+print("===== number =====")
 count = 100
 count_type = type(count)
 print("count", count, count_type)
@@ -14,15 +15,35 @@ print(result1, result2)
 print("===== string =====")
 
 course = "AI Python Fullstack"
+
 result = type(course)
-print(f"the type of course: {result}")
+print(f"the type of course (1): {result}")
+
 result = course.title()
-print(f"the result:", {result})
+print(f"the result (2):", {result})
 
 result = course.upper()
-print(f"the result:", {result})
+print(f"the result (3):", {result})
 
-result11 = count
+result = course.replace("Fullstack", "MasterClass")
+print(f"the result (4): {result}")
 
-result11 = 30
-print(count)
+print("===== boolean =====")
+
+# function > type() input() bool() int() str()
+y = input("Give your value for y:")
+print("y:", y)
+
+result = y.isnumeric()
+print(f"the input value is numeric: {result}")
+
+# TRUTHY VS FALSY value
+# TRUTHY: True, 100, -100. "MIT"
+# FALSY: False 0 " NONE"
+#
+
+test_falsy = "" or False or None or 0 or 100
+print("The Falsy:, bool(test_falsy)")
+
+test_truthy = "" or False or None or 0 or 100
+print("The Truthy:, bool(test_truthy)")
