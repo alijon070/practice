490b74cd19a98fa62075c7c2be3d461383156a9c
Albert <muminovali648@gmail.com>
2026-04-15 23:41:02 +0900

    feat: learn string in python

--- primitive.py
+++ primitive.py
@@ -1,4 +1,4 @@
-print("==========")
+print("===== number =====")
 # in JAVA, variable is a name storage location!
 # in Python, vRIbles is anmed reference!
 
@@ -10,3 +10,19 @@ print(f"the count: {count} and type: {count_type}")
 result1 = count.bit_count()  # method
 result2 = count.numerator  # state
 print(result1, result2)
+
+print("===== string =====")
+
+course = "AI Python Fullstack"
+result = type(course)
+print(f"the type of course: {result}")
+result = course.title()
+print(f"the result:", {result})
+
+result = course.upper()
+print(f"the result:", {result})
+
+result11 = count
+
+result11 = 30
+print(count)
