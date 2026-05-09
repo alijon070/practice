026955b4abd4b5428760875131028f0fd3468335
Albert <muminovali648@gmail.com>
2026-05-10 04:12:31 +0900

    feat: MITASK: K-TASK

--- train.py
+++ train.py
@@ -1,9 +1,33 @@
+'''K-TASK (PYTHON)
+
+Shunday function yozing, u string qabul qilsin va string ichidagi eng uzun sozni qaytarsin.
+MASALAN: find_longest("I come from Uzbekistan ") return "Uzbekistan"
+'''
+b = "I come from Uzbekistan Afghanistan SouthKoreaofNorth"
+
+
+def find_longest(a):
+    c = a.split()
+    print(c)
+    length = 0
+    value = ""
+    for word in c:
+        if len(word) > length:
+            length = len(word)
+            value = word
+
+    return value
+
+
+result = find_longest(b)
+print(result)
+
 ''' I-TASK (PYTHON)
 
 Shunday function tuzing, unga string argument pass bolsin. Function ushbu agrumentdagi digitlarni yangi stringda return qilsin
 MASALAN: get_digits("m14i1t") return qiladi "141"
 '''
-
+'''
 nums = []
 
 
@@ -17,3 +41,4 @@ def get_digits(a):
 
 result = get_digits("m14i1t")
 print(result)
+'''
